465ff1e1ccc8bdee8613bb725defb1c5dbd7d511
Thiago Ribeiro Antunes <108041782+ThiagoRantunes@users.noreply.github.com>
2025-04-07 13:32:19 +0000

    alteração ou quase isso

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 function linkNavigate(link){
     window.location = link;
 }
+
+function openCard(id){
+    const card = document.getElementById(id);
+    if(card.classList.contains("block")){
+        card.classList.replace("block", "card-text");
+    }else{
+        card.classList.add("card-text","block");
+    }
+}
